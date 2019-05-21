d58754a8de7a434f773da139e2c3022f7fef1abc
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-05-21 07:54:57 -0400

    add .js extension

---
+++ MDNJS/grammar-types.js
@@ -0,0 +1,9 @@
+//May 21
+//Didn't realize the three different variable names.
+// var - declare a variable
+// let - block-scoped local variable
+// const - block scoped, *read-only* named constant
+// 
+
+var a;
+console.log('The value of a is ' + a); //Value of a is undefined
