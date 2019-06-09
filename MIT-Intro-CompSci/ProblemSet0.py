8f44508745fab594102f58b4892f9f1524d771d4
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-06-09 17:43:07 -0400

    Still trying to figure out the modules and extensions not working

--- MIT-Intro-CompSci/ProblemSet0.py
+++ MIT-Intro-CompSci/ProblemSet0.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('/user/local/lib/python3.7/site-packages/numpy/')
-print(sys.path)
+# print(sys.path)
 import numpy as np
 
 
@@ -10,13 +10,17 @@ import numpy as np
 # print oTime.isoformat()
 
 
-x = input("Enter a number for x: ")
-
-y = input("Enter a number for y: ")
+xStr = input("Enter a number for x: ")
+x = int(xStr)
+yStr = input("Enter a number for y: ")
+y = int(yStr)
 xSq = x**y
-print('The power of x to y is ', xSq)
-xLog = np.log2(x)
-print('The log base 2 of x is ', xLog)
+print ("The power of x to y is " + str(xSq))
+
+
+
+# xLog = np.log2(x)
+# print('The log base 2 of x is ', xLog)
 
 #print('The power of ', x, ' to ', y, ' is ', x**y)
 #print("The log of x is ", log2(x))
