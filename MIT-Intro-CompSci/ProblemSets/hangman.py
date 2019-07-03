52bacd6c9d53a564b5831207a419edf248de8bb2
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-07-03 07:52:57 -0400

    Fixed sorting and get_guessed_word

--- MIT-Intro-CompSci/ProblemSets/hangman.py
+++ MIT-Intro-CompSci/ProblemSets/hangman.py
@@ -60,7 +60,7 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
-    for char in range(secret_word):
+    for char in secret_word:
       if char in letters_guessed:
         return True
       else:
@@ -89,8 +89,10 @@ def get_available_letters(letters_guessed):
       yet been guessed.
     '''
 
-    unguessed_letters = string.ascii_lowercase - letters_guessed
-    return(unguessed_letters)
+    unguessed_letters = set(string.ascii_lowercase).difference(letters_guessed)
+    unguessed_letters = list(unguessed_letters)
+    unguessed_letters.sort()
+    return(print("Available letters: " + ''.join(unguessed_letters)))
     
     
 
@@ -123,26 +125,38 @@ def hangman(secret_word):
     letter_warnings = 4
     letters_guessed = []
     print("Welcome to Hangman. You have been sentenced to die. Unless you correctly guess the word I am thinking of.")
-    print("You have " + guesses_left + "guesses left. Use them wisely")
-    print("The mystery word has " + len(secret_word) + " letters.")
-    print("Letters to choose: " + get_available_letters(letters_guessed))
+    print("You have " + str(guesses_left) + " guesses left. Use them wisely")
+    print("The mystery word has " + str(len(secret_word)) + " letters.")
+    print("Letters to choose: " + string.ascii_lowercase)
     print("-----")
     
    
     new_guess = input("What letter do you choose?")
     for char in new_guess:
-      if char.isalpha:
-        new_guess = new_guess.lower
+      if char.isalpha():
+        new_guess = new_guess.lower()
+        print("You guessed " + str(new_guess))
       else:
         print("That isn't a letter.")
-        print("You've got " + letter_warnings + "warnings left. Don't push me.")
+        print("You've got " + str(letter_warnings) + " warnings left. Don't push me.")
         letter_warnings -= 1
         if letter_warnings <= 0:
           guesses_left -= 1
     letters_guessed.append(new_guess)
+    print(secret_word) # print secret word to see if code is correct
+
+    if new_guess in secret_word:
+      print("There is a " + str(new_guess) + " in the word!")
+    else:
+      print("Sorry. There is not a " + str(new_guess) + " in the word.")
+    guesses_left -= 1
     is_word_guessed(secret_word, letters_guessed)
+    print("You now have " + str(guesses_left) + " guesses left.")
     get_guessed_word(secret_word, letters_guessed)
-    # NOT SURE IF RIGHT> STEP THROUGH MENTALLY NEXT TIME
+    get_available_letters(letters_guessed)
+
+    #LETTERS TO CHOOSE NOT PRINTING IN ABC ORDER
+    #AFTER INITIAL LETTER GUESS, program ends.
 
 
 
