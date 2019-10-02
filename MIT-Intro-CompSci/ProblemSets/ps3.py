bc3a5631242ec3f94d2b9d14606ec7b6e5fa6086
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-10-02 07:35:21 -0400

    Start implementation of wildcard in is_valid_word

--- MIT-Intro-CompSci/ProblemSets/ps3.py
+++ MIT-Intro-CompSci/ProblemSets/ps3.py
@@ -233,11 +233,15 @@ def is_valid_word(word, hand, word_list):
     word = word.lower()
     #check for presence of wildcard
     if '*' in word:
-        #find position of *
-        #slice string at * position
-        #iterate through VOWELS string, replacing * with vowel
-        #run through rest of the function for word checK
-    #make sure word (as a string) is in word_list
+        #iterate through vowels
+        for char in VOWELS:
+            #replace '*' with vowels from list
+            wildcard_guess = word.replace('*', char)
+            #check if wildcard_guess in word_list
+            if wildcard_guess in word_list:
+                #if yes, convert to dict (for scoring)
+                word = get_frequency_dict(word)
+                #need to make sure the char vowel is NOT added to the score
     if word in word_list:
         #convert word string to word dict
         word = get_frequency_dict(word)
