56927079978330ab0fc848abeb5563044b203c42
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-12-04 22:26:06 -0500

    Finish day 1 (2nd puzzle)

--- MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-1.py
+++ MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-1.py
@@ -1,4 +1,6 @@
 # Fuel required to launch modules is ((mass / 3)[rounded down] - 2)
+# NEW REQUIREMENT - FUEL also needs fuel. Need to make the equation recursive? 
+
 # fuel-input.txt is text file with masses of all modules (1 per line)
 
 
@@ -23,7 +25,14 @@ def module_fuel_calc(mass):
     '''
     fuel_required = math.floor(mass/3)
     fuel_required = fuel_required - 2
-    return fuel_required
+    total_fuel = fuel_required
+
+    while fuel_required > 8:
+        fuel_required = math.floor(fuel_required/3)
+        fuel_required = fuel_required - 2
+        total_fuel += fuel_required
+    else:
+        return total_fuel
 
 #print(text_file)
 #print(module_masses)
@@ -32,7 +41,7 @@ total_fuel = 0
 # for each line of fuel-input.txt
 for mass in module_masses:
     # run function
-    print(total_fuel)
+    #print(total_fuel)
     # add result of each function to total-fuel
     total_fuel += module_fuel_calc(mass)
 print("Total fuel needed is", total_fuel)
