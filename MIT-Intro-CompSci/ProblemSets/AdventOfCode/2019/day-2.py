ca8f9b693c52ff7a539e4fd5fdb1f3f8a50d7760
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-12-12 07:54:59 -0500

    Finish day 2. Working!

--- MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-2.py
+++ MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-2.py
@@ -30,23 +30,24 @@ for index, opcode in enumerate(opcodes_list):
         # opcode 1 means addition
         if opcode == 1:
             # get the values is opcode pos 1 and pos 2
-            num1 = int(opcodes_list[index + 1])
-            num2 = int(opcodes_list[index + 2])
-            print("Num 1 is", num1)
-            print("Num 2 is", num2)
+            num1 = int(opcodes_list[int(opcodes_list[index + 1])])
+            num2 = int(opcodes_list[int(opcodes_list[index + 2])])
+            print("Num 1 is", num1, "index position", opcodes_list[index + 1])
+            print("Num 2 is", num2,"index position", opcodes_list[index + 2])
             # calculate sum of opcode 1
             opcode_result = num1 + num2
             print("Opcode result", opcode_result)
             # find opcode save pos
             save_pos = int(opcodes_list[index + 3])
-            print(save_pos)
+            print("Save position is", save_pos)
             # save opcode sum in save pos
             opcodes_list[save_pos] = opcode_result
+            print("---")
         # opcdoe 2 means multiplication
         if opcode == 2:
             # get the values in opcode pos 1 and pos 2
-            num1 = int(opcodes_list[index + 1])
-            num2 = int(opcodes_list[index + 2])
+            num1 = int(opcodes_list[int(opcodes_list[index + 1])])
+            num2 = int(opcodes_list[int(opcodes_list[index + 2])])
             print("Num 1 is", num1)
             print("Num 2 is", num2)
             # calculate product of opcode 2
@@ -54,9 +55,11 @@ for index, opcode in enumerate(opcodes_list):
             print("Opcode result", opcode_result)
             # find opcode save pos
             save_pos = int(opcodes_list[index + 3])
-            print(save_pos)
+            print("Save position is", save_pos)
             # save opcode product in save pos
             opcodes_list[save_pos] = opcode_result
+            print("---")
         if opcode == 99:
-            quit
-print(opcodes_list)
+            print("Program stopping")
+            print("---")
+            break
