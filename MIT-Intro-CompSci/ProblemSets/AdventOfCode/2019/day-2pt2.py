4ab01d1baa208978575e8afeab9a491fbbee5ece
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-12-17 08:08:36 -0500

    Fix iterating through number groups. Way to complex, need to look at simpler solutions.

--- MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-2pt2.py
+++ MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-2pt2.py
@@ -1,4 +1,4 @@
-
+import itertools
 
 def opcode_calc(opcode, index):
     '''
@@ -36,6 +36,7 @@ def opcode_calc(opcode, index):
         #print("Opcode result", opcode_result)
         # find opcode save pos
         save_pos = int(opcodes_list[index + 3])
+        print("Save pos:",save_pos)
         #print("Save position is", save_pos)
         # save opcode product in save pos
         opcodes_list[save_pos] = opcode_result
@@ -51,10 +52,12 @@ def noun_verb_iter ():
     noun = []
     verb = []
     for x in range(100):
-        noun.append()
-        verb.append()
-    combined = [(noun, verb) for x in noun for y in second] 
-    print(combined)
+        #print(x)
+        noun.append(x)
+        verb.append(x)
+    num_combos = list(itertools.product(noun, verb))
+    return num_combos
+
 
 
 # open text file
@@ -64,24 +67,28 @@ opcodes_raw = opcodes.read()
 # split at ","
 opcodes_list = []
 opcodes_list = opcodes_raw.split(",")
-noun = 0
-verb = 0
+num_combos = noun_verb_iter()
+#print("Num combos:", num_combos)
 #print(opcodes_list) #Getting the raw opcode_list for debugging
 # use enumerate in order to relate a specific opcode to its index
 while opcodes_list[0] != 19690720:
     opcodes_list = opcodes_raw.split(",")
-    opcodes_list[1] = noun
-    opcodes_list[2] = verb
-    if noun == verb:
-        noun += 1
-    else: 
-        verb += 1
-    for index, opcode in enumerate(opcodes_list):
-        opcode = int(opcode)
-        #look at index pos 0 and every 4th index
-        if index % 4 == 0:
-            #print("Index is", index)
-            #print("Opcode is", opcode)
-            opcode_calc(opcode, index)
-    print(opcodes_list[0])
-print(opcodes_list[0:5])
+    print("Opcodes list",opcodes_list)
+    for values in num_combos:
+        print("Values in num_combo:",values)
+        opcodes_list[1] = int(values[0])
+        print("Values pos 0:", values[0])
+        print("Opcode list 1:",opcodes_list[1])
+        opcodes_list[2] = int(values[1])
+        print("Value pos 1:", values[1])
+        print("Opcodes list pos 2:", opcodes_list[2])
+    #print("Opcodes list:", opcodes_list)
+        for index, opcode in enumerate(opcodes_list):
+            opcode = int(opcode)
+            #look at index pos 0 and every 4th index
+            if index % 4 == 0:
+                #print("Index is", index)
+                #print("Opcode is", opcode)
+                opcode_calc(opcode, index)
+        print("Opcodes list pos 0->5:",opcodes_list[0:5])
+print("Opcodes list pos 0->5:", opcodes_list[0:5])
