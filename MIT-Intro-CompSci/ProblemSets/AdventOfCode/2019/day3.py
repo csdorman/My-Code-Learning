2071e6f23e57182184d948b6d22a5dadbaf982b1
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-12-19 11:52:12 -0500

    Add day 3 files

---
+++ MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day3.py
@@ -0,0 +1,4 @@
+
+# Using "taxicab geometry" (grid): https://en.wikipedia.org/wiki/Taxicab_geometry
+# Reddit hints for day 3: https://www.reddit.com/r/adventofcode/comments/e5bz2w/2019_day_3_solutions/
+
