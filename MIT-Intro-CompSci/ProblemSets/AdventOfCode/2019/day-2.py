2c9479a9990fd13947605c4428fe20916299ff67
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2019-12-19 11:46:27 -0500

    Finish day 2.

--- MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-2.py
+++ MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-2.py
@@ -1,93 +1,55 @@
-
-def opcode_calc(opcode, index):
+def open_file(file):
     '''
-    Used to calculate the opcode result.
-    
+    Open a specific file and split into a list at ","
+    '''
+    opcodes = open(file)
+    opcodes_raw = opcodes.read()
+    opcodes_list = map(int, opcodes_raw.split(','))
+    return list(opcodes_list)
+
+
+def opcode_program(opcodes_list, noun, verb):
+    '''
+    Used to calculate the opcode result.   
+
     Opcode = 0, 1, or 99
+
     Index = The index position of the opcode. Used to find the other parameters based on their relation to the index number
     '''
-    if opcode == 99:
-        quit
-    elif opcode == 1:
-        # get the values is opcode pos 1 and pos 2
-        param1 = int(opcodes_list[int(opcodes_list[index + 1])])
-        param2 = int(opcodes_list[int(opcodes_list[index + 2])])
-        print("Parameter 1 is", param1, ", the index position", opcodes_list[index + 1])
-        print("Parameter 2 is", param2,", the index position", opcodes_list[index + 2])
-        # calculate sum of opcode 1
-        opcode_result = param1 + param2
-        print("Opcode result", opcode_result)
-        # find opcode save pos
-        save_pos = int(opcodes_list[index + 3])
-        print("Save position is", save_pos)
-        # save opcode sum in save pos
-        opcodes_list[save_pos] = opcode_result
-        print("---")
-    elif opcode == 2:
-         # get the values in opcode pos 1 and pos 2
-        param1 = int(opcodes_list[int(opcodes_list[index + 1])])
-        param2 = int(opcodes_list[int(opcodes_list[index + 2])])
-        print("Parameter 1 is", param1)
-        print("Parameter 2 is", param2)
-        # calculate product of opcode 2
-        opcode_result = param1 * param2
-        print("Opcode result", opcode_result)
-        # find opcode save pos
-        save_pos = int(opcodes_list[index + 3])
-        print("Save position is", save_pos)
-        # save opcode product in save pos
-        opcodes_list[save_pos] = opcode_result
-        print("---")
+    opcodes_list[1] = noun
+    opcodes_list[2] = verb
+    counter = 0
+    while counter < len(opcodes_list):
+        opcode = opcodes_list[counter]
+        param1 = opcodes_list[opcodes_list[counter + 1]]
+        param2 = opcodes_list[opcodes_list[counter + 2]]
+        dest = opcodes_list[counter + 3]
+        if opcode == 1:
+            opcodes_list[dest] = param1 + param2
+            counter += 4
+        elif opcode == 2:
+            opcodes_list[dest] = param1 * param2
+            counter += 4
+        elif opcode == 99:
+            break
+        else:
+            print("Code unknown")
+            break
+    return opcodes_list[0]
 
-# open text file
-opcodes = open("opcode-list.txt")
-# read text file 
-opcodes_raw = opcodes.read()
-# split at ","
-opcodes_list = []
-opcodes_list = opcodes_raw.split(",")
-#print(opcodes_list) #Getting the raw opcode_list for debugging
-# use enumerate in order to relate a specific opcode to its index
-for index, opcode in enumerate(opcodes_list):
-    opcode = int(opcode)
-    #look at index pos 0 and every 4th index
-    if index % 4 == 0:
-        print("Index is", index)
-        print("Opcode is", opcode)
-        opcode_calc(opcode, index)
-    #     # opcode 1 means addition
-    #     if opcode == 1:
-    #         # get the values is opcode pos 1 and pos 2
-    #         param1 = int(opcodes_list[int(opcodes_list[index + 1])])
-    #         param2 = int(opcodes_list[int(opcodes_list[index + 2])])
-    #         #print("Parameter 1 is", param1, "index position", opcodes_list[index + 1])
-    #         #print("Parameter 2 is", param2,"index position", opcodes_list[index + 2])
-    #         # calculate sum of opcode 1
-    #         opcode_result = param1 + param2
-    #         #print("Opcode result", opcode_result)
-    #         # find opcode save pos
-    #         save_pos = int(opcodes_list[index + 3])
-    #         #print("Save position is", save_pos)
-    #         # save opcode sum in save pos
-    #         opcodes_list[save_pos] = opcode_result
-    #         print("---")
-    #     # opcdoe 2 means multiplication
-    #     if opcode == 2:
-    #         # get the values in opcode pos 1 and pos 2
-    #         param1 = int(opcodes_list[int(opcodes_list[index + 1])])
-    #         param2 = int(opcodes_list[int(opcodes_list[index + 2])])
-    #         #print("Parameter 1 is", param1)
-    #         #print("Parameter 2 is", param2)
-    #         # calculate product of opcode 2
-    #         opcode_result = param1 * param2
-    #         #print("Opcode result", opcode_result)
-    #         # find opcode save pos
-    #         save_pos = int(opcodes_list[index + 3])
-    #         #print("Save position is", save_pos)
-    #         # save opcode product in save pos
-    #         opcodes_list[save_pos] = opcode_result
-    #         print("---")
-    #     if opcode == 99:
-    #         print("Program stopping")
-    #         print("---")
-    #         break
+def part_one():
+    return opcode_program(open_file("opcode-list.txt"), 12, 2)
+
+def part_two(output):
+    '''
+    Part two of Day 2
+    '''
+    opcodes_list = open_file("opcode-list.txt")
+    for noun in range(100):
+        for verb in range(100):
+            if opcode_program(opcodes_list.copy(), noun, verb) == output:
+                return 100 * noun + verb
+    return "not found"
+
+print(part_one())
+print(part_two(19690720))
