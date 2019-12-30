ff5c8af9719426686ede3ddb397f68ab7d697143
Colin Dorman <csdorman@Colins-MBP.attlocal.net>
2019-12-30 10:03:40 -0500

    Start working on second part of day 4 pt 1 problem. First draft, throws error.

--- MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-4.py
+++ MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day-4.py
@@ -21,9 +21,26 @@ def password_options_adjacent(low, high):
                 index = int(index)
                 index += 1
     print(len(pw_adjacent_options))
-    print(pw_adjacent_options)
+    pw_options_increasing(pw_adjacent_options)
+                
+def pw_options_increasing(pw_adjacent_options):
+    '''
+    - Take the list from the previous problem
+    - Check to make sure all numbers are increasing
+    '''
+    pw_possibilities = []
+    for option in pw_adjacent_options:
+        num_string = str(option)
+        for index, num in enumerate(num_string):
+            if num(index) > num(index + 1):
+                pass
+            else:
+                pw_possibilities.append()
+    print(len(pw_possibilities))
+
+            
                 
 
-#def password_options_increasing()
+# pw_options_increasing should run first. It will eliminate more options quicker
 
-password_options_adjacent(146810, 612564)
+password_options_adjacent(146810, 156810)
