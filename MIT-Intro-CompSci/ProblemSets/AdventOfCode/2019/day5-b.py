c2521b723652bf5ed048ca9e7f871c4402753ee5
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-01-20 07:27:27 -0500

    Start on param_mode. Infinite loop in line 14-16

--- MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day5-b.py
+++ MIT-Intro-CompSci/ProblemSets/AdventOfCode/2019/day5-b.py
@@ -7,37 +7,44 @@ def engage_computer(raw_data):
     intcode_list = map(int,raw_data.split(','))
     return list(intcode_list)
 
-#def param_mode(incode_list):
+def param_mode(intcode_list):
+    intcodes = list(map(str, intcode_list))
+    for code in intcodes:
+        print(len(code))
+        if len(code) < 5:
+            #for i in range(5-len(code)):
+            intcodes.insert(0,"0")
+    return(intcodes)
 
 # define different opcode instructions
-def opcode_func(intcode_list):
+def opcode_func(intcode_list, input_inst):
     counter = 0
     print(len(intcode_list))
     while counter < len(intcode_list):
         opcode = intcode_list[counter]
         param1 = intcode_list[intcode_list[counter+1]]
         param2 = intcode_list[intcode_list[counter+2]]
-        dest = intcode_list[counter + 3]
+        param3 = intcode_list[counter + 3]
         if opcode == 1:
             # add params
-            intcode_list[dest] = param1 + param2
+            intcode_list[param3] = param1 + param2
             print("Opcode 1 triggered")
             counter += 4
         elif opcode == 2:
             # multiply params
-            intcode_list[dest] = param1 * param2
+            intcode_list[param3] = param1 * param2
             counter += 4
         elif opcode == 3:
-            # take input and save to only parameter
-            intcode_list[dest] = param1
+            # take input and save to only parameter 
+            intcode_list[param1] = input_inst
             counter += 2
         elif opcode == 4:
             # output the value of the only parameter
-            return intcode_list[dest]
+            return intcode_list[param1]
             counter += 2
         else:
             break
         return intcode_list
 
 
-print(engage_computer("diagnostic-code.txt"))
+print(param_mode(engage_computer("diagnostic-code.txt")))
