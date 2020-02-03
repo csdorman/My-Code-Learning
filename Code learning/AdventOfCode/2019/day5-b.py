984e5923d35faa105bb47b9d25ad920275e437f4
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-02-03 14:20:35 -0500

    Commenting all code, removing nonfunctional code.

--- Code learning/AdventOfCode/2019/day5-b.py
+++ Code learning/AdventOfCode/2019/day5-b.py
@@ -3,12 +3,13 @@
 #open intcode file
 raw_data = open("diagnostic-code.txt", "r")
 raw_data = raw_data.read()
-#print(raw_data)
 intcode_list = raw_data.split(',')
+
 #turn intcode strings into integers
 for i in range(len(intcode_list)):
     intcode_list[i] = int(intcode_list[i])
 
+#turn all opcodes into 5 digits
 def intcode_modes(opcode):
     #intcodes_list = list(map(str, intcode_list))
     opcode = str(opcode)
@@ -21,6 +22,7 @@ def intcode_modes(opcode):
     intcode.append(opcode)
     return(opcode)
 
+#get intcode mode (parameter/immediate) based on opcode
 def mode(mode,position):
     val = 0
     #immediate mode
@@ -31,6 +33,7 @@ def mode(mode,position):
         val = intcode_list[intcode_list[position]]
     return val
 
+#run computer
 def intcode_computer(codes, input_num):
     counter = 0
     while counter < len(codes):
@@ -70,50 +73,4 @@ def intcode_computer(codes, input_num):
             #print(codes)
             break
     
-
-
-
-# define different opcode instructions
-def opcode_func(intcode_list, input_inst):
-    counter = 0
-    #print(intcode_list)
-    while counter < len(intcode_list):
-        #set opcode to counter
-        opcode_inst = str(intcode_list[counter])
-        #set params to operate by default in position mode
-        param1 = int(intcode_list[counter+1])
-        param2 = int(intcode_list[counter+2])
-        param3 = int(intcode_list[counter+3])
-        for opcode in intcode_list:
-            opcode_inst = intcode_modes(opcode_inst)
-        if str(opcode_inst[-1]) == "1":
-            #addition
-            intcode_list[param3] = int(mode_value(opcode_inst[2], param1, intcode_list)) + int(mode_value(opcode_inst[1], param2, intcode_list))
-            #print("Opcode 1 triggered")
-            counter += 4
-        elif str(opcode_inst[-1]) == "2":
-            # multiply params
-            intcode_list[param3] = int(mode_value(opcode_inst[2], param1, intcode_list)) * int(mode_value(opcode_inst[1], param2, intcode_list))
-            counter += 4
-        elif str(opcode_inst[-1]) == "3":
-            # take input and save to only parameter 
-            intcode_list[param1] = int(input_inst)
-            #print("Opcode 3 triggered")
-            counter += 2
-        elif str(opcode_inst[-1]) == "4":
-            # output the value of the only parameter
-            input_inst = param1
-            counter += 2
-        else:
-            print("Counter:", counter, "Intcode:", opcode_inst)
-            print("IF statement not reached")
-            counter += 1
-    return intcode_list
-   
-
-#print(data("diagnostic-code.txt"))
-
 intcode_computer(intcode_list, 1)
-
-
-#print(param_mode(engage_computer("diagnostic-code.txt")))
