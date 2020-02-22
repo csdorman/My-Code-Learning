89f934214b06eeb8c6b047b0780354ebf978bc60
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-02-22 14:12:09 -0500

    Day 6 pt1
    Restarting indirect orbit func from scratch. Not working correctly

--- AdventOfCode/2019/day6.py
+++ AdventOfCode/2019/day6.py
@@ -10,23 +10,47 @@ def direct_orbit_count(orbit_data):
         direct_orbits += 1
     return direct_orbits
 
+def indirect_orbit_count_2(orbit_data):
+    #counter for the orbit to compare with
+    orbit_num = 0
+    #counter for the orbit to compare against
+    orbit_count = 1
+    #indirect orbits for a specific index
+    local_count = 0
+    #indirect orbit total
+    total_count = 0
+    print("num",orbit_num, "count",orbit_count, "local", local_count, "total", total_count)
+    for orbit_num in range(len(orbit_data)):
+        for orbit in orbit_data:
+            print(orbit_num, orbit)
+
 def indirect_orbit_count(orbit_data):
     local_indirect = 0
     total_indirect = 0
     #index to compare with
     orbit_num = 0
     orbit_compare = 1
-    while orbit_num < len(orbit_data):
-        print(orbit_num)
+    #while orbit_compare < len(orbit_data):
+    print("orbit",orbit_num, orbit_compare)
+    print("orbit data len", len(orbit_data))
+    for orbit_num in range(len(orbit_data)):
+    #while orbit_compare < len(orbit_data):
         for orbit in orbit_data:
-            if orbit_data[orbit_compare-1][-1] == orbit_data[orbit_compare][0]:
+            print(orbit)
+            if orbit[-1] == orbit_data[orbit_compare][0]:
                 local_indirect += 1
+                print("orbits",local_indirect, total_indirect)
+                print("pass",orbit_compare)
                 orbit_compare += 1
             else:
+                orbit_compare += 1
                 total_indirect = local_indirect
+                print("fail", orbit_compare)
         orbit_num += 1
     return (total_indirect)
 
+    #There are 11 direct and 31 indirect orbits in the test
+    #for a total of 42 orbits
 
     #NEED TO REDO WITH RECURSION(?) MATH IS WRONG
     #compare orbit[0][-1] with orbit[1][0]
@@ -42,4 +66,4 @@ def indirect_orbit_count(orbit_data):
 #print orbit data
 print(orbit_data)
 print(direct_orbit_count(orbit_data))
-print(indirect_orbit_count(orbit_data))
+print(indirect_orbit_count_2(orbit_data))
