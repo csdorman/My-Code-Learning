465a4bd9b8c4a87f727e24902401be453a9be224
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-02-27 10:21:22 -0500

    Day6 pt 1
    Problem seems to be only checking for consecutive orbits matching.

--- AdventOfCode/2019/day6.py
+++ AdventOfCode/2019/day6.py
@@ -21,14 +21,16 @@ def indirect_orbit_count(orbit_data):
     prev_counter = counter - 1
     #indirect orbits
     indirect_orbits = 0
+    #print(counter, current_item)
     #initial comparison
     while counter > 0:
-        #print(orbit_data[counter][:3], orbit_data[prev_counter][-3:])
+        print(orbit_data[counter][:3]) 
+        print(orbit_data[prev_counter][-3:])
         if orbit_data[counter][:3] == orbit_data[prev_counter][-3:]:
             indirect_orbits += 1
             while prev_item > -1:
                 if orbit_data[current_item][:3] == orbit_data[prev_item][-3:]:
-                    print(current_item, prev_item)
+                    #git print(current_item, prev_item)
                     indirect_orbits += 1
                     current_item -= 1
                     prev_item = current_item - 1
