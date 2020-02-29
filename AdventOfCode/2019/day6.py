0f83e505c9dcc705a5f526657df86ff601f4a096
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-02-29 13:49:41 -0500

    Day 6 pt 1
    Working to slice the orbit data by symbol ')' rather than simply by string numbers.
    This is a bit more complex than I though - still not getting test data results to equal 42.

--- AdventOfCode/2019/day6.py
+++ AdventOfCode/2019/day6.py
@@ -1,5 +1,5 @@
 #load orbit data
-raw_data = open("data6.txt", "r")
+raw_data = open("test-data6.txt", "r")
 raw_data = raw_data.read()
 orbit_data = raw_data.splitlines()
 
@@ -24,35 +24,40 @@ def indirect_orbit_count(orbit_data):
     #print(counter, current_item)
     #initial comparison
     while counter > 0:
-        print(orbit_data[counter][:3]) 
-        print(orbit_data[prev_counter][-3:])
-        if orbit_data[counter][:3] == orbit_data[prev_counter][-3:]:
+        #print(orbit_data[counter][:3]) 
+        #print(orbit_data[prev_counter][-3:])
+        orbit_loc = orbit_data[counter].find(')')
+        orbit_loc_prev = orbit_data[prev_counter].find(')')
+        if orbit_data[counter][:orbit_loc] == orbit_data[prev_counter][orbit_loc_prev+1:]:
             indirect_orbits += 1
-            while prev_item > -1:
-                if orbit_data[current_item][:3] == orbit_data[prev_item][-3:]:
-                    #git print(current_item, prev_item)
-                    indirect_orbits += 1
-                    current_item -= 1
-                    prev_item = current_item - 1
-                else:
-                    current_item -= 1
-                    prev_item = current_item - 1
+            orbit_loc = orbit_data[current_item].find(')')
+            orbit_loc_prev = orbit_data[prev_item].find(')')
+            if orbit_data[current_item][:orbit_loc] == orbit_data[prev_item][orbit_loc_prev+1:]:
+                #git print(current_item, prev_item)
+                indirect_orbits += 1
+                current_item -= 1
+                prev_item = current_item - 1
+            else:
+                current_item -= 1
+                prev_item = current_item - 1
         else:
+            orbit_loc = orbit_data[current_item].find(')')
+            orbit_loc_prev = orbit_data[prev_item].find(')')
             while prev_item > -1:
-                if orbit_data[current_item][:3] == orbit_data[prev_item][-3:]:
+                if orbit_data[current_item][:orbit_loc] == orbit_data[prev_item][orbit_loc_prev+1:]:
                         indirect_orbits += 1
                         current_item -= 1
                         prev_item = current_item - 1 
                 else:
                     current_item -= 1
                     prev_item = current_item - 1
+        print(orbit_data[counter][:orbit_loc], orbit_data[prev_counter][orbit_loc_prev+1:])
         counter -= 1
         prev_counter = counter - 1
         current_item = counter - 1
         prev_item = current_item - 1
     return(indirect_orbits)
 
-
-
 #print orbit data
+#print(direct_orbit_count(orbit_data)) #not needed now
 print(indirect_orbit_count(orbit_data))
